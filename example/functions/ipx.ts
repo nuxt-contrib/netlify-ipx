2a8338969594e1ba2f049f986cdd8d001810487c
Pooya Parsa <pyapar@gmail.com>
2021-07-01 23:34:57 +0200

    use package name in example

--- example/functions/ipx.ts
+++ example/functions/ipx.ts
@@ -1,4 +1,4 @@
-import { createIPXHandler } from '../../src/function'
+import { createIPXHandler } from 'netlify-plugin-ipx'
 
 export const handler = createIPXHandler({
   domains: ['images.unsplash.com']
